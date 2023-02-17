22c909d37ce423c06be81578b38e1b2a6df035d4
Otchere Turkson <otchereturkson@gmail.com>
2023-02-17 15:37:52 -0800

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ * main - main blocks
  * Description: Get a random number and check its last digit, compare it with 5
  * Retun: 0 (success)
  */
