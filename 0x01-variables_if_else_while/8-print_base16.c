be3a144e14bd070e946f016baa52923ceeab97b6
Otchere Turkson <otchereturkson@gmail.com>
2023-02-17 17:37:13 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - block
+ * Description: prints all single digit number of base 10
+ * Starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
