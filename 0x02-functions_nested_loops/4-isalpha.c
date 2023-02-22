419d3b4a596e6ef11d27a5f56e900a1cd4160d8b
Otchere Turkson <otchereturkson@gmail.com>
2023-02-21 17:23:24 -0800

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,17 @@
 /**
  * _isalpha -Checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else
+ * Return: Always 0.
  */
 
 int _isalpha(int c)
 
 {
-	if ((c >= 65 && <= 90) || ( c >= 97 && c <= 122))
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
