c3413c6a4d38fdaeaa438cb061fb459043e2d63c
Otchere Turkson <otchereturkson@gmail.com>
2023-02-21 20:44:51 -0800

    updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,17 +9,4 @@ void jack_beauer(void)
 	i = 0;
 	while (i < 24)
 	{
-		j = 0;
-		while (j < 60)
-	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-		j++;
-	}
-		i++;
-	}
-}
+		_putchar((i /10) + '0')
